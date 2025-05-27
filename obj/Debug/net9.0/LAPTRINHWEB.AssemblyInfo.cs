e9035790f944d79ada2be0e616292a32ffb4ea36
bao diep gia <baodiepgia@baos-MacBook-Pro.local>
2025-05-27 12:30:32 +0700

    tour

--- obj/Debug/net9.0/LAPTRINHWEB.AssemblyInfo.cs
+++ obj/Debug/net9.0/LAPTRINHWEB.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LAPTRINHWEB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15da5d387ec4401b357fb6104ec57c3001002473")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e824cd5d256d6b972752da6d85fb934e33f5ca46")]
 [assembly: System.Reflection.AssemblyProductAttribute("LAPTRINHWEB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LAPTRINHWEB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
