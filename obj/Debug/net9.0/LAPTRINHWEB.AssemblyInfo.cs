7ed5b22c280aed0c0e4dad4b4e7f28b58fefe032
baomap456 <126744704+baomap456@users.noreply.github.com>
2025-06-14 09:36:02 +0700

    ..

--- obj/Debug/net9.0/LAPTRINHWEB.AssemblyInfo.cs
+++ obj/Debug/net9.0/LAPTRINHWEB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LAPTRINHWEB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2849e79e29f96b0942e84156024829d470b18a14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7131929223b0917877da07921a3a2daf76f4132")]
 [assembly: System.Reflection.AssemblyProductAttribute("LAPTRINHWEB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LAPTRINHWEB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
